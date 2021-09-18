80b2262b2e5196fe65c90709895f0de9e8604f01
gianmv <gianmove@gmail.com>
2021-09-17 21:15:54 -0500

    Add simple string compresion

--- Chapter01/stringCompression.py
+++ Chapter01/stringCompression.py
@@ -1,15 +1,16 @@
 def string_compression(s: str) -> str:
     counter = 1
-    last_letter = ""
+    last_letter = "" + s[0]
     final_string = ""
-    for letter in s:
-        if letter == last_letter:
-            counter +=1
-        else:
-            counter = 1
-            final_string += last_letter + str(counter) 
+    for letter in s[1:]:
+        if last_letter != letter:
+            final_string += last_letter + str(counter)
             last_letter = letter
+            counter = 1
+        else:
+            counter += 1
 
+    final_string += last_letter + str(counter)
     if len(final_string) < len(s):
         return final_string
     return s
