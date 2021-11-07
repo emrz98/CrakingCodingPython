fa1263b8060500589c45348e90646a640a4e4556
Emmanuel <emrz98@gmail.com>
2021-11-06 19:05:04 -0600

    add validation for len in string rotation

--- Chapter01/string_rotation.py
+++ Chapter01/string_rotation.py
@@ -1,5 +1,7 @@
 
 
 def string_rotation(s1, s2):
-    word = s2 + s2
-    return s1 in word
+    if len(s1) == len(s2) and len(s1)>0:
+        word = s2 + s2
+        return s1 in word
+    return False
